3f2ff94a7c23de245c6d4a9498496f8f19f4fd89
N3therSoul <davidecuber@gmail.com>
2016-07-29 16:08:48 +0200

    Update Perzona.java

--- Perzona.java
+++ Perzona.java
@@ -3,26 +3,26 @@
  * KLASSE PER RAPPRESENTARE UNA PERZONA (FALZA)
  * Ringrazio la community di GH per i numerosi suggerimenti!
  * 
- * IMPORTANTE LA KLASSE KANE, CHE NON Ë DELLA KASTA {@link Kane}
+ * IMPORTANTE LA KLASSE KANE, CHE NON √® DELLA KASTA {@link Kane}
  */
 public class Perzona implements Comparable<Kane> {
 	boolean perzona;
-	int et‡;
+	int et√†;
 	/**
-	 * LE PERZONE ANNO L'ET‡ E SN VERE O FALZE
-	 * @param perzona SE Ë VERA O FALZA
-	 * @param et‡
+	 * LE PERZONE ANNO L'ET√† E SN VERE O FALZE
+	 * @param perzona SE √® VERA O FALZA
+	 * @param et√†
 	 */
-	public Perzona(boolean perzona, int et‡) {
+	public Perzona(boolean perzona, int et√†) {
 		this.perzona = perzona;
-		this.et‡ = et‡;
+		this.et√† = et√†;
 	}
 	/**
 	 * LE PERZONE KE NON CONOSCIAMO SN FALZE HA PRIORI
 	 */
 	public Perzona() {
 		perzona = false;
-		et‡ = 40;
+		et√† = 40;
 	}
 	/**
 	 * METODO CONTRO LA KASTA PER TROVARE LE PERZONE FALZE
@@ -35,24 +35,25 @@ public class Perzona implements Comparable<Kane> {
 	 * METODO PER FARSI PERDONARE, MA LI KANI SN MEJO
 	 */
 	public void REDIMI() {
-	    /*la perzona falsa kiede skusa e diventa kome i kani, solo un pÚ  peggio*/
+	    /*la perzona falsa kiede skusa e diventa kome i kani, solo un p√≤  peggio*/
 	    this.perzona = true;
 	}
 	/**
 	 * KONFRONTA LE PERZONE KON I KANI
+	 * ORA KONFRONTA ANKE KON LA KORRETTA NAMING KONVENTIONZ
 	 * @return I KANI SN KOMUNCUE MEJO
 	 */
-	public String KonfrontaConKani() {
+	public String konfrontaConKani() {
 		if(perzona==false)
 			return "SN MEJO I KANID DELE PERZONE FALSE1!1!!";
 		return "I KANI SONO MEJO DELE PERZONE11!!1";
 	}
 	/**
-	 * GUARDA L'ET‡ DI UNA PERZONA
-	 * @return et‡
+	 * GUARDA L'ET√† DI UNA PERZONA
+	 * @return et√†
 	 */
-	public int getEt‡() {
-		return et‡;
+	public int getEt√†() {
+		return et√†;
 	}
 	/**
 	 * KOMPARA I KANI CON LE PERZONE, MA SONO MGLIO I KANI11!!
@@ -66,11 +67,11 @@ public class Perzona implements Comparable<Kane> {
 	}
 	/**
 	 * BUONGIORNISSIMO!!!11!111!
-	 * @return kaffË?????
+	 * @return kaff√®?????
 	 */
-	public String KaffË() {
-		if(this.getEt‡()>=40)
-			return "Buongiornissimooo kaffËËËËË?!!? 1!";
+	public String kaff√®√®√®√®√®() {
+		if(this.getEt√†()>=40)
+			return "Buongiornissimooo kaff√®√®√®√®√®?!!? 1!";
 		return null;
 	}
 }
